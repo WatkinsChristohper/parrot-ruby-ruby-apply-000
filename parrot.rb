415cfeaa18f2265d6681a6b73f345c91cdc75fe0
Christopher Watkins <chriswatkins7@yahoo.com>
2020-10-05 04:46:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-phrase = "Squawk!"
-
-def parrot(string ="Squawk!")
-  puts "#{string}"
-  " #{string}"
+def parrot (bird = "Pretty bird!")
+  puts "#{bird}"
+  bird
 end
+
+def parrot (squawk = "Squawk!")
+  puts "#{squawk}"
+  squawk
+end 
